b0da250221ffe2d5825d090a24e704a6ccd904df
Pier-Luc Michaud <pluc.git@outlook.com>
2017-08-06 23:38:14 -0400

    Polybar run script updated (forgot to commit with the config changes)

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
 killall polybar
-polybar bar1 -c ~/.dotfiles/polybar/config &
-polybar bar2 -c ~/.dotfiles/polybar/config &
-polybar topbar1 -c ~/.dotfiles/polybar/config &
-polybar topbar2 -c ~/.dotfiles/polybar/config &
+polybar top -c ~/.dotfiles/polybar/config &
+polybar bottom -c ~/.dotfiles/polybar/config &
