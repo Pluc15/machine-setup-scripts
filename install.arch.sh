7c74e2aa4cf2d7985fa5dd38c6e60f83fe94fe7c
Pier-Luc Michaud <pluc.git@outlook.com>
2017-08-30 19:06:31 -0400

    Improve install script; Add missing packages; Add xorg.conf linking

--- install.arch.sh
+++ install.arch.sh
@@ -1,4 +1,9 @@
-sudo pacman -S \
+#!/bin/sh
+
+sudo rm /etc/pacman.d/mirrorlist
+sudo ln -fs $HOME/.dotfiles/pacman/mirrorlist /etc/pacman.d/mirrorlist
+
+sudo pacman -Sq --needed --noconfirm \
 fish \
 curl \
 wget \
@@ -20,6 +25,7 @@ redshift \
 scrot \
 imagemagick \
 xdialog \
+dialog \
 python-setuptools \
 rofi \
 gimp \
@@ -28,85 +34,93 @@ qt4 \
 thunar \
 xorg-xrandr \
 arandr \
-compton
-
-git clone https://aur.archlinux.org/i3-gaps.git ~/.aur/i3-gaps
-pushd ~/.aur/i3-gaps
-makepkg -si
-popd
-
-git clone https://aur.archlinux.org/google-chrome.git ~/.aur/google-chrome
-pushd ~/.aur/google-chrome
-makepkg -si
-popd
-
-git clone https://aur.archlinux.org/spotify.git ~/.aur/spotify
-pushd ~/.aur/spotify
-makepkg -si
-popd
-
-git clone https://aur.archlinux.org/visual-studio-code.git ~/.aur/visual-studio-code
-pushd ~/.aur/visual-studio-code
-makepkg -si
-popd
-
-git clone https://aur.archlinux.org/x2vnc-no-xinerama.git ~/.aur/x2vnc-no-xinerama
-pushd ~/.aur/x2vnc-no-xinerama
-makepkg -si
-popd
-
-git clone https://aur.archlinux.org/ttf-font-awesome.git ~/.aur/ttf-font-awesome
-pushd ~/.aur/ttf-font-awesome
-makepkg -si
-popd
-
-git clone https://aur.archlinux.org/discord.git ~/.aur/discord
-pushd ~/.aur/discord
-makepkg -si
-popd
-
-git clone https://aur.archlinux.org/slack-desktop.git ~/.aur/slack-desktop
-pushd ~/.aur/slack-desktop
-makepkg -si
-popd
-
-git clone https://aur.archlinux.org/fisherman.git ~/.aur/fisherman
-pushd ~/.aur/fisherman
-makepkg -si
-popd
-
-git clone https://aur.archlinux.org/pkgbase/skypeforlinux-bin ~/.aur/skypeforlinux-bin
-pushd ~/.aur/skypeforlinux-bin
-makepkg -si
-popd
-
-git clone https://aur.archlinux.org/polybar-git.git ~/.aur/polybar-git
-pushd ~/.aur/polybar-git
-makepkg -si
-popd
-
-git clone https://aur.archlinux.org/python-pywal.git ~/.aur/python-pywal
-pushd ~/.aur/python-pywal
-makepkg -si
-popd
+compton \
+networkmanager \
+networkmanager-openvpn \
+networkmanager-pptp \
+network-manager-applet \
+xorg \
+xf86-video-intel \
+xdg-utils \
+xclip \
+vlc \
+rxvt-unicode \
+gnome-keyring \
+fzf \
+cifs-utils \
+openssl \
+openssh
 
 fisher z fzf
 
-mkdir -p ~/.config/i3
-ln -fs ~/.dotfiles/i3/config ~/.config/i3/config
+git clone https://aur.archlinux.org/i3-gaps.git $HOME/.aur/i3-gaps
+pushd $HOME/.aur/i3-gaps
+makepkg -si --noconfirm
+popd
 
-mkdir -p ~/.config/dunst
-ln -fs ~/.dotfiles/dunst/dunstrc ~/.config/dunst/dunstrc
+git clone https://aur.archlinux.org/google-chrome.git $HOME/.aur/google-chrome
+pushd $HOME/.aur/google-chrome
+makepkg -si --noconfirm
+popd
 
-ln -fs ~/.dotfiles/xorg/.Xresources ~/.Xresources
+git clone https://aur.archlinux.org/spotify.git $HOME/.aur/spotify
+pushd $HOME/.aur/spotify
+makepkg -si --noconfirm
+popd
 
-ln -fs ~/.dotfiles/xorg/.xinitrc ~/.xinitrc
+git clone https://aur.archlinux.org/visual-studio-code.git $HOME/.aur/visual-studio-code
+pushd $HOME/.aur/visual-studio-code
+makepkg -si --noconfirm
+popd
 
-ln -fs ~/.dotfiles/fish/config.fish ~/.config/fish/config.fish
+git clone https://aur.archlinux.org/x2vnc-no-xinerama.git $HOME/.aur/x2vnc-no-xinerama
+pushd $HOME/.aur/x2vnc-no-xinerama
+makepkg -si --noconfirm
+popd
 
-ln -fs ~/.dotfiles/compton/compton.conf ~/.config/compton.conf
+git clone https://aur.archlinux.org/ttf-font-awesome.git $HOME/.aur/ttf-font-awesome
+pushd $HOME/.aur/ttf-font-awesome
+makepkg -si --noconfirm
+popd
 
-ln -fs ~/.dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
+git clone https://aur.archlinux.org/discord.git $HOME/.aur/discord
+pushd $HOME/.aur/discord
+makepkg -si --noconfirm
+popd
 
-sudo rm /etc/pacman.d/mirrorlist
-sudo ln -fs ~/.dotfiles/pacman/mirrorlist /etc/pacman.d/mirrorlist
+git clone https://aur.archlinux.org/slack-desktop.git $HOME/.aur/slack-desktop
+pushd $HOME/.aur/slack-desktop
+makepkg -si --noconfirm
+popd
+
+git clone https://aur.archlinux.org/fisherman.git $HOME/.aur/fisherman
+pushd $HOME/.aur/fisherman
+makepkg -si --noconfirm
+popd
+
+git clone https://aur.archlinux.org/pkgbase/skypeforlinux-bin $HOME/.aur/skypeforlinux-bin
+pushd $HOME/.aur/skypeforlinux-bin
+makepkg -si --noconfirm
+popd
+
+git clone https://aur.archlinux.org/polybar-git.git $HOME/.aur/polybar-git
+pushd $HOME/.aur/polybar-git
+makepkg -si --noconfirm
+popd
+
+git clone https://aur.archlinux.org/python-pywal.git $HOME/.aur/python-pywal
+pushd $HOME/.aur/python-pywal
+makepkg -si --noconfirm
+popd
+
+mkdir -p $HOME/.config/i3
+mkdir -p $HOME/.config/dunst
+
+ln -fs $HOME/.dotfiles/i3/config $HOME/.config/i3/config
+ln -fs $HOME/.dotfiles/dunst/dunstrc $HOME/.config/dunst/dunstrc
+ln -fs $HOME/.dotfiles/xorg/.Xresources $HOME/.Xresources
+ln -fs $HOME/.dotfiles/xorg/.xinitrc $HOME/.xinitrc
+ln -fs $HOME/.dotfiles/xorg/.xserverrc $HOME/.xserverrc
+ln -fs $HOME/.dotfiles/fish/config.fish $HOME/.config/fish/config.fish
+ln -fs $HOME/.dotfiles/compton/compton.conf $HOME/.config/compton.conf
+ln -fs $HOME/.dotfiles/vscode/settings.json $HOME/.config/Code/User/settings.json
--- update.arch.sh
+++ update.arch.sh
@@ -1,63 +1,64 @@
-sudo pacman -Syu
-
-pushd ~/.aur/i3-gaps
-git pull
-makepkg -si
-popd
-
-pushd ~/.aur/google-chrome
-git pull
-makepkg -si
-popd
-
-pushd ~/.aur/spotify
-git pull
-makepkg -si
-popd
-
-pushd ~/.aur/visual-studio-code
-git pull
-makepkg -si
-popd
-
-pushd ~/.aur/x2vnc-no-xinerama
-git pull
-makepkg -si
-popd
-
-pushd ~/.aur/ttf-font-awesome
-git pull
-makepkg -si
-popd
-
-pushd ~/.aur/discord
-git pull
-makepkg -si
-popd
-
-pushd ~/.aur/slack-desktop
-git pull
-makepkg -si
-popd
-
-pushd ~/.aur/fisherman
-git pull
-makepkg -si
-popd
-
-pushd ~/.aur/skypeforlinux-bin
-git pull
-makepkg -si
-popd
-
-pushd ~/.aur/polybar-git
-git pull
-makepkg -si
-popd
-
-pushd ~/.aur/python-pywal
-git pull
-makepkg -si
-popd
+#!/bin/sh
+sudo pacman -Syuq --noconfirm --needed
 
 fisher up
+
+pushd $HOME/.aur/i3-gaps
+git pull
+makepkg -si --noconfirm --needed
+popd
+
+pushd $HOME/.aur/google-chrome
+git pull
+makepkg -si --noconfirm --needed
+popd
+
+pushd $HOME/.aur/spotify
+git pull
+makepkg -si --noconfirm --needed
+popd
+
+pushd $HOME/.aur/visual-studio-code
+git pull
+makepkg -si --noconfirm --needed
+popd
+
+pushd $HOME/.aur/x2vnc-no-xinerama
+git pull
+makepkg -si --noconfirm --needed
+popd
+
+pushd $HOME/.aur/ttf-font-awesome
+git pull
+makepkg -si --noconfirm --needed
+popd
+
+pushd $HOME/.aur/discord
+git pull
+makepkg -si --noconfirm --needed
+popd
+
+pushd $HOME/.aur/slack-desktop
+git pull
+makepkg -si --noconfirm --needed
+popd
+
+pushd $HOME/.aur/fisherman
+git pull
+makepkg -si --noconfirm --needed
+popd
+
+pushd $HOME/.aur/skypeforlinux-bin
+git pull
+makepkg -si --noconfirm --needed
+popd
+
+pushd $HOME/.aur/polybar-git
+git pull
+makepkg -si --noconfirm --needed
+popd
+
+pushd $HOME/.aur/python-pywal
+git pull
+makepkg -si --noconfirm --needed
+popd
