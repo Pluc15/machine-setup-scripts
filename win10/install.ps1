900a160c22d446172610024ffc80b07d57757443
Pier-Luc Michaud <pier-luc.michaud@mediaclip.ca>
2017-11-27 12:59:17 -0500

    Dont link VSCode settings

--- win10/install.ps1
+++ win10/install.ps1
@@ -3,12 +3,6 @@
 $env:Dotfiles = (Get-Item -Path ".\" -Verbose).FullName
 [Environment]::SetEnvironmentVariable("Dotfiles", $env:Dotfiles, "User")
 
-# Link vscode configs
-$DotfilesVsCodeSettingsPath = Join-Path $env:Dotfiles "cross\vscode\settings.json"
-$LinkVsCodeSettingsPath = "$env:AppData\Code\User\settings.json"
-Remove-Item -Path $LinkVsCodeSettingsPath
-New-Item -Path $LinkVsCodeSettingsPath -ItemType SymbolicLink -Value $DotfilesVsCodeSettingsPath
-
 # Choco
 Set-ExecutionPolicy AllSigned; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install `
