3c5f81e586709b0cd7ac51be360a1785ddb54f19
Pier-Luc Michaud <pier-luc.michaud@mediaclip.ca>
2018-03-28 12:41:18 -0400

    Add .gitconfig and link for windows installer

--- win10/install.ps1
+++ win10/install.ps1
@@ -21,3 +21,9 @@ $DotfilesConEmuSettingsPath = Join-Path $env:Dotfiles "win10\conemu\ConEmu.xml"
 $LinkConEmuSettingsPath = "$env:AppData\ConEmu.xml"
 Remove-Item -Path $LinkConEmuSettingsPath
 New-Item -Path $LinkConEmuSettingsPath -ItemType SymbolicLink -Value $DotfilesConEmuSettingsPath
+
+# Link git config
+$DotfilesGitconfigPath = Join-Path $env:Dotfiles "git\.gitconfig"
+$LinkGitconfigPath = "$env:Home\.gitconfig"
+Remove-Item -Path $LinkGitconfigPath
+New-Item -Path $LinkGitconfigPath -ItemType SymbolicLink -Value $DotfilesGitconfigPath
