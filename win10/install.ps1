56b10370290cf1858a5efda00e595ff1f4ac69aa
Pier-Luc Michaud <pier-luc.michaud@mediaclip.ca>
2018-03-28 12:44:40 -0400

    Remove choco

--- win10/install.ps1
+++ win10/install.ps1
@@ -3,18 +3,6 @@
 $env:Dotfiles = (Get-Item -Path ".\" -Verbose).FullName
 [Environment]::SetEnvironmentVariable("Dotfiles", $env:Dotfiles, "User")
 
-# Choco
-Set-ExecutionPolicy AllSigned; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install `
-fzf `
-git `
-git.install `
-gitextensions `
-paint.net `
-greenshot `
-nodejs-lts `
-dotnetcore-sdk `
-poshgit
 
 # Link ConEmu config
 $DotfilesConEmuSettingsPath = Join-Path $env:Dotfiles "win10\conemu\ConEmu.xml"
