abeb5eecf0bb4f5fea348c87cdfff98785e5316c
Pier-Luc Michaud <pier-luc.michaud@mediaclip.ca>
2018-03-28 20:56:12 -0400

    Make install.sh install only (no longer used to update) Added update script Symlink fish profile instead of echo into it Symlink .gitconfig User packer instead of manually installing AUR packages Get i3-gaps from pacman instead of AUR

--- arch/install.sh
+++ arch/install.sh
@@ -6,7 +6,6 @@ source $DOTFILES/arch/install.fn.sh
 
 sudosafeln $DOTFILES/arch/pacman/mirrorlist /etc/pacman.d/mirrorlist
 
-sudo pacman -Syuq --noconfirm --needed
 sudo pacman -Sq --needed --noconfirm \
 fish \
 curl \
@@ -64,31 +63,27 @@ gcc \
 libmariadbclient \
 mariadb
 
-install_aur i3-gaps
-install_aur google-chrome
-install_aur spotify
-install_aur visual-studio-code
-install_aur x2vnc-no-xinerama
-install_aur ttf-font-awesome
-install_aur slack-desktop
-install_aur fisherman
-install_aur skypeforlinux-bin
-install_aur polybar-git
-install_aur python-pywal
-install_aur openoffice
+install_aur packer
 
-install_aur libc++
-install_aur discord
-
-install_aur lttng-ust
-install_aur dotnet-host
-install_aur dotnet-runtime-2.0
-install_aur dotnet
-install_aur dotnet-sdk-2.0
+sudo packer -Sq --needed --noconfirm \
+google-chrome \
+spotify \
+visual-studio-code \
+x2vnc-no-xinerama \
+ttf-font-awesome \
+slack-desktop \
+fisherman \
+polybar-git \
+python-pywal \
+openoffice \
+discord \
+dotnet-sdk
 
 fish -c fisher z fzf
 fish -c fisher up
 
+# Configs
+safeln $DOTFILES/linux/fish/config.fish $HOME/.config/fish/config.fish
 safeln $DOTFILES/linux/i3/config $HOME/.config/i3/config
 safeln $DOTFILES/linux/dunst/dunstrc $HOME/.config/dunst/dunstrc
 safeln $DOTFILES/linux/xorg/.Xresources $HOME/.Xresources
@@ -98,7 +93,9 @@ safeln $DOTFILES/linux/xorg/xorg.conf $HOME/.config/xorg.conf
 safeln $DOTFILES/linux/compton/compton.conf $HOME/.config/compton.conf
 safeln $DOTFILES/linux/polybar/config $HOME/.config/polybar/config
 safeln $DOTFILES/cross/vscode/settings.json $HOME/.config/Code/User/settings.json
+safeln $DOTFILES/git/.gitconfig $HOME/.gitconfig
 
+# Global scripts
 safeln $DOTFILES/linux/scripts/git-tree.sh $HOME/.scripts/git-tree.sh
 safeln $DOTFILES/linux/scripts/kb-layout-toggle.sh $HOME/.scripts/kb-layout-toggle.sh
 safeln $DOTFILES/linux/scripts/lock.sh $HOME/.scripts/lock.sh
@@ -109,5 +106,3 @@ safeln $DOTFILES/linux/scripts/rofi-launch.sh $HOME/.scripts/rofi-launch.sh
 safeln $DOTFILES/linux/scripts/rofi-modi-lpass.sh $HOME/.scripts/rofi-modi-lpass.sh
 safeln $DOTFILES/linux/scripts/spotify-current-song.sh $HOME/.scripts/spotify-current-song.sh
 safeln $DOTFILES/linux/scripts/wallpaper.sh $HOME/.scripts/wallpaper.sh
-
-echo "source $DOTFILES/linux/fish/profile.fish" > $HOME/.config/fish/config.fish
---
+++ arch/update.sh
@@ -0,0 +1,2 @@
+sudo pacman -Syuq --noconfirm --needed
+packer -Syuq --noconfirm --needed
