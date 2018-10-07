d0f756f01e4655f265545396f3ab568963e90d46
Pier-Luc Michaud <pier-luc.michaud@mediaclip.ca>
2018-10-06 22:55:20 -0400

    Add pacman shortcuts

--- fish/config.fish
+++ fish/config.fish
@@ -7,5 +7,10 @@ alias ggt "git-tree"
 alias ggs "git status"
 alias ggph "git push"
 alias ggpl "git pull"
+
+alias paci "pacman -Rsn $argv"
 alias pacr "pacman -Rsn $argv"
 alias pacl "pacman -Qqe"
+alias pacuall "pacman -Syu"
+alias paclaur "pacman -Qm"
+
