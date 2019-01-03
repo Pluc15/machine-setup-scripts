0d6118257868beac5ae6e3c3ab61c2c53d9dcb2f
Pier-Luc Michaud <pluc.m@outlook.com>
2019-01-03 13:36:19 -0500

    Remove wsl specific fish profile

--- wsl/profile.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/fish
-set -x PATH $PATH /mnt/d/Dev/scripts/wsl
-set -x PLUC_SCRIPTS /mnt/d/Dev/scripts/wsl
-alias gs='git status'
-alias gt='sh $PLUC_SCRIPTS/git-tree.sh'
-
-#function fish_prompt
-#    echo -ne $argv
-#end
