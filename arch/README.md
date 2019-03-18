6dff39a4e5f6f40797e0c6a0b45c4b7755704db2
Pier-Luc Michaud <pluc.m@outlook.com>
2019-03-17 20:14:49 -0400

    Rework i3 arch setup (WIP)

--- arch/README.md
+++ arch/README.md
@@ -1,3 +1,17 @@
+# Arch installation notes
+
+## Pre chroot
+
+* Setup insternet (eth interface up + dhcpd)
+* Get pacman mirrorlist from this git
+
+## Post chroot
+
+* Install sudoer
+* Add user (with home folder)
+* Add new user to sudoers
+* Remove root login
+
 # Installing
 
 ```bash
@@ -12,3 +26,10 @@ cd ~/.dotfiles
 git pull
 sh ~/.dotfiles/arch/install.sh
 ```
+
+# TODOs
+
+* Learn xorg config (and remake)
+* Theme dunst, gtk, qt, i3blocks
+* Configure ranger
+* Fix background for i3lock (needs png)
--- arch/install.sh
+++ arch/install.sh
@@ -2,120 +2,121 @@
 
 DOTFILES=`dirname $0`/..
 DOTFILES=`realpath $DOTFILES`
-
 source $DOTFILES/bash/functions/install_helpers.sh
 
-echoStep "Linking pacman mirror files"
+echoStep "Link pacman mirrorlist"
 sudosafeln $DOTFILES/arch/pacman_mirrorlist /etc/pacman.d/mirrorlist
 
-echoStep "Updating official repository packages"
-sudo pacman -Syuq --noconfirm --needed
+echoStep "Updating pacman packages"
+sudo pacman -Syu
 
-echoStep "Installing new official repository packages"
-sudo pacman -Sq --needed --noconfirm \
-fish \
-curl \
-wget \
-elinks \
-xorg \
-pulseaudio \
-pavucontrol \
-playerctl \
-i3lock \
-dunst \
-freerdp \
-pptpclient \
-ranger \
-base-devel \
-git \
-fzf \
-feh \
-redshift \
-scrot \
-imagemagick \
-xdialog \
-dialog \
-python-setuptools \
-rofi \
-gimp \
-transmission-qt \
-qt4 \
-thunar \
-tumbler \
-xorg-xrandr \
-arandr \
-compton \
-networkmanager \
-networkmanager-openvpn \
-networkmanager-pptp \
-network-manager-applet \
-xorg \
-xf86-video-intel \
-xdg-utils \
-xclip \
-vlc \
-rxvt-unicode \
-gnome-keyring \
-fzf \
-cifs-utils \
-openssl \
-openssh \
-lastpass-cli \
-p7zip \
-libreoffice-fresh \
-x11vnc \
-cmake \
-make \
-gcc \
-libmariadbclient \
-mariadb \
-ttf-font-awesome \
-python-pywal \
-dotnet-sdk
+echoStep "Installing pacman packages"
+sudo pacman -S \
+    arandr \
+    base-devel \
+    cifs-utils \
+    compton \
+    curl \
+    dotnet-sdk \
+    dunst \
+    elinks \
+    feh \
+    fish \
+    freerdp \
+    fzf \
+    gimp \
+    git \
+    gtk2 \
+    gtk3 \
+    i3-gaps \
+    i3blocks \
+    i3lock \
+    imagemagick \
+    lastpass-cli \
+    libreoffice-fresh \
+    mesa \
+    networkmanager \
+    networkmanager-openvpn \
+    networkmanager-pptp \
+    openssh \
+    openssl \
+    otf-font-awesome \
+    p7zip \
+    pacman-contrib \
+    pavucontrol \
+    pulseaudio \
+    qt4 \
+    qt5-base \
+    ranger \
+    redshift \
+    rofi \
+    rxvt-unicode \
+    scrot \
+    smbclient \
+    sudo \
+    thunar \
+    tk \
+    transmission-qt \
+    ttf-font-awesome \
+    ttf-fira-code \
+    ttf-fira-mono \
+    ttf-hack \
+    ttf-roboto \
+    tumbler \
+    vlc \
+    wget \
+    x11vnc \
+    xclip \
+    xdg-utils \
+    xdialog \
+    xorg-font-util \
+    xorg-fonts-100dpi \
+    xorg-fonts-75dpi \
+    xorg-fonts-encodings \
+    xorg-server \
+    xorg-setxkbmap \
+    xorg-xbacklight \
+    xorg-xinput \
+    xorg-xprop \
+    xorg-xrandr \
+    xorg-xrefresh
 
-if [ ! -d "$HOME/.aur/packer-aur" ]
-then
-	echoStep "Installing packer-aur"
-	git clone https://aur.archlinux.org/packer-aur.git $HOME/.aur/$1
-	pushd $HOME/.aur/packer-aur
-else
-	echoStep "Updating packer-aur"
-	pushd $HOME/.aur/packer-aur
-	git pull
+echoStep "Install / Update yay"
+mkdir -p $HOME/.aur
+pushd $HOME/.aur
+if [ ! -d "yay" ]; then
+    git clone https://aur.archlinux.org/yay.git
 fi
-makepkg -si --noconfirm --needed
+cd yay
+git pull
+makepkg -si
 popd
 
-echoStep "Updating AUR packages"
-packer-aur -Syuq --noconfirm
-
-echoStep "Installing new AUR packages"
-packer-aur -S --noconfirm \
-google-chrome \
-spotify \
-visual-studio-code-bin \
-x2vnc-no-xinerama \
-fisherman \
-discord
-
-echoStep "Updating fish plugins"
-fish -c fisher up
-
-echoStep "Installing new fish plugins"
-fish -c fisher z fzf
+echoStep "Install / Update yay packages"
+yay -Syu
 
 echoStep "Linking configuraion files"
 safeln $DOTFILES/i3/config $HOME/.config/i3/config
-safeln $DOTFILES/i3/i3status.config $HOME/.config/i3status/config
-safeln $DOTFILES/dunst/dunstrc $HOME/.config/dunst/dunstrc
+safeln $DOTFILES/i3/i3blocks.config $HOME/.config/i3blocks/config
 safeln $DOTFILES/xorg/.Xresources $HOME/.Xresources
 safeln $DOTFILES/xorg/.xinitrc $HOME/.xinitrc
 safeln $DOTFILES/xorg/.xserverrc $HOME/.xserverrc
 safeln $DOTFILES/xorg/xorg.conf $HOME/.config/xorg.conf
-safeln $DOTFILES/compton/compton.conf $HOME/.config/compton.conf
 safeln $DOTFILES/git/.gitconfig $HOME/.gitconfig
+safeln $DOTFILES/gtk/gtk3.ini $HOME/gtk-3.0/settings.ini
+safeln $DOTFILES/gtk/.gtkrc-2.0 $HOME/.gtkrc-2.0
+safeln $DOTFILES/dunst/dunstrc $HOME/.config/dunst/dunstrc
+safeln $DOTFILES/compton/compton.conf $HOME/.config/compton.conf
 
-echoStep "Linking Fish profiles"
+echoStep "Linking scripts"
+safeln $DOTFILES/bash/scripts/kb-layout-toggle $HOME/.scripts/kb-layout-toggle
+safeln $DOTFILES/bash/scripts/power-menu $HOME/.scripts/power-menu
+safeln $DOTFILES/bash/scripts/rofi-launch $HOME/.scripts/rofi-launch
+safeln $DOTFILES/bash/scripts/spotify-current-song $HOME/.scripts/spotify-current-song
+safeln $DOTFILES/bash/scripts/wallpaper $HOME/.scripts/wallpaper
+
+echoStep "Building fish profile"
 echo "set DOTFILES $DOTFILES" > $HOME/.config/fish/config.fish
-echo "source $HOME/.config/fish/base.fish" >> $HOME/.config/fish/base.fish
-echo "source $HOME/.config/fish/arch.fish" >> $HOME/.config/fish/arch.fish
+echo "source $DOTFILES/fish/base.fish" >> $HOME/.config/fish/config.fish
+echo "source $DOTFILES/fish/arch.fish" >> $HOME/.config/fish/config.fish
+
