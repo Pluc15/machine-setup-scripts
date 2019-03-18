26b0a658c2ca061529052985537fdf60be05897d
Pier-Luc Michaud <pluc.m@outlook.com>
2019-03-17 20:08:40 -0400

    Seperate fish profiles properly

--- fish/arch.fish
+++ fish/arch.fish
@@ -2,8 +2,13 @@ if [ -z "$DISPLAY" ]; and [ -n "$XDG_VTNR" ]; and [ "$XDG_VTNR" -eq 1 ]
   exec startx
 end
 
-alias paci "pacman -S $argv"
-alias pacr "pacman -Rsn $argv"
-alias pacl "pacman -Qqe"
-alias pacuall "pacman -Syu"
-alias paclaur "pacman -Qm"
+function fish_prompt
+	set_color normal
+	echo -n (hostname)
+	echo -n ": "
+	set_color 0F0
+	echo (pwd)
+	set_color 999
+	echo -n '> '
+	set_color normal
+end
--- fish/wsl.fish
+++ fish/wsl.fish
@@ -1,3 +1,14 @@
 if test -n "$SHELLROOT"
 	cd "$SHELLROOT"
 end
+
+function fish_prompt
+	set_color normal
+	echo -n (hostname)
+	echo -n ": "
+	set_color 0F0
+	echo (pwd)
+	set_color 999
+	echo -n 'WSL> '
+	set_color normal
+end
