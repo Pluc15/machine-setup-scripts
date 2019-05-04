84ebad47ebee57ad8ae4b852ad7e4cc75a1cb7df
Pier-Luc Michaud <pluc.m@outlook.com>
2019-05-04 16:10:34 -0400

    Fix some boot issue with xresources

--- manjaro/README.md
+++ manjaro/README.md
@@ -7,3 +7,5 @@
     - Visual Sutido Code
     - GTK 2/3
     - QT
+- Prevent pulseaudio to go over 100% (PA_VOLUME_UI_MAX?)
+- Fix / add icons in Polybar
--- sh_scripts/toggle-split-monitor.sh
+++ sh_scripts/toggle-split-monitor.sh
@@ -5,8 +5,11 @@ then
     xrandr --delmonitor HDMI-0_p1
     xrandr --delmonitor HDMI-0_p2
     xrandr --setmonitor HDMI-0_p1 3440/819x1440/346+0+0 HDMI-0
-else
+elif [[ $p2monitor == *"HDMI-0_p1"* ]]
+then
     xrandr --delmonitor HDMI-0_p1
     xrandr --setmonitor HDMI-0_p1 2560/609x1440/346+0+0 HDMI-0
     xrandr --setmonitor HDMI-0_p2 880/210x1440/346+2560+0 none
+else
+    xrandr --setmonitor HDMI-0_p1 3440/819x1440/346+0+0 HDMI-0
 fi
