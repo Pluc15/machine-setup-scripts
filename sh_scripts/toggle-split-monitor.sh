409357c4229232fed20b8123c47b987a06c4a27c
Pier-Luc Michaud <pluc.m@outlook.com>
2019-05-04 21:52:10 -0400

    Modify split monitor so that not running it at boot doesnt break everything

--- sh_scripts/toggle-split-monitor.sh
+++ sh_scripts/toggle-split-monitor.sh
@@ -2,14 +2,10 @@
 p2monitor=`xrandr --listmonitors`
 if [[ $p2monitor == *"HDMI-0_p2"* ]]
 then
-    xrandr --delmonitor HDMI-0_p1
     xrandr --delmonitor HDMI-0_p2
-    xrandr --setmonitor HDMI-0_p1 3440/819x1440/346+0+0 HDMI-0
-elif [[ $p2monitor == *"HDMI-0_p1"* ]]
+    xrandr --setmonitor HDMI-0 3440/819x1440/346+0+0 HDMI-0
+elif [[ $p2monitor == *"HDMI-0"* ]]
 then
-    xrandr --delmonitor HDMI-0_p1
-    xrandr --setmonitor HDMI-0_p1 2560/609x1440/346+0+0 HDMI-0
+    xrandr --setmonitor HDMI-0 2560/609x1440/346+0+0 HDMI-0
     xrandr --setmonitor HDMI-0_p2 880/210x1440/346+2560+0 none
-else
-    xrandr --setmonitor HDMI-0_p1 3440/819x1440/346+0+0 HDMI-0
 fi
