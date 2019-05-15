0bdaf4d686d12fc9ce3df033a60a08a669a7d189
Pier-Luc Michaud <pluc.m@outlook.com>
2019-05-15 19:35:12 -0400

    Restructure everything, again

---
+++ configs/.profile
@@ -0,0 +1,3 @@
+. ~/.config/dotfiles
+
+#TODO Health Check
--- sh_scripts/update-pywal-scheme.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cp "$HOME/.cache/wal/.Xresources"             "$DOTFILES/configs/.Xresources"
-cp "$HOME/.cache/wal/dunstrc"                 "$DOTFILES/configs/dunstrc"
-cp "$HOME/.cache/wal/rofi-my-dark-theme.rasi" "$DOTFILES/configs/rofi-my-dark-theme.rasi"
-reload
