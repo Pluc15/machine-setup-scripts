4a82858bb8e94f84eee6da8e1ab74bc5729b74f7
Pier-Luc Michaud <pluc.m@outlook.com>
2019-07-17 19:00:29 -0400

    Update tamper monkey scripts so it doesnt modify to page structure. Instead, only add css to make the player exactly 1 full screen and scroll down to it so it looks like fullscreen yet allows you to scroll down and use the rest of the site

--- tampermonkey/CrunchyrollFullscreen.user.js
+++ tampermonkey/CrunchyrollFullscreen.user.js
@@ -5,71 +5,35 @@
 // @match        *://www.crunchyroll.com/*/*
 // @grant        none
 // ==/UserScript==
-(function() {
-  console.log("= Crunchyroll Fullscreen Loaded =");
-  function setFullscreen() {
-    var player = document.getElementById("vilos-player");
-
-    if (player) {
-      var body = document.querySelector("body");
-      body.style.overflow = "hidden";
-
-      player.style.position = "absolute";
-      player.style.top = 0;
-      player.style.right = 0;
-      player.style.bottom = 0;
-      player.style.left = 0;
-      player.style.zIndex = 999998;
-      player.parentElement.removeChild(player);
-      body.appendChild(player);
-
-      var controls = document.createElement("div");
-      controls.style.position = "absolute";
-      controls.style.bottom = 0;
-      controls.style.left = "40%";
-      controls.style.right = "40%";
-      controls.style.backgroundColor = "black";
-      controls.style.fontSize = "20px";
-      controls.style.fontWeight = "bold";
-      controls.style.zIndex = 999999;
-      body.appendChild(controls);
-
-      var oldPrev = document.querySelector(
-        ".collection-carousel-media-link-current"
-      ).parentElement.previousElementSibling;
-      var prevUrl = oldPrev ? oldPrev.querySelector("a").href : null;
-
-      if (prevUrl) {
-        var prev = document.createElement("a");
-        prev.style.position = "relative";
-        prev.style.bottom = 0;
-        prev.style.left = 0;
-        prev.style.color = "white";
-        prev.href = prevUrl;
-        prev.textContent = "<<";
-        controls.appendChild(prev);
-      }
-
-      var oldNext = document.querySelector(
-        ".collection-carousel-media-link-current"
-      ).parentElement.nextElementSibling;
-      var nextUrl = oldNext ? oldNext.querySelector("a").href : null;
-
-      if (nextUrl) {
-        var next = document.createElement("a");
-        next.style.position = "relative";
-        next.style.bottom = 0;
-        next.style.right = 0;
-        next.style.color = "white";
-        next.href = nextUrl;
-        next.textContent = ">>";
-        controls.appendChild(next);
-      }
-    } else {
-      console.log("Video frame not found, trying again in 500ms");
-      setTimeout(setFullscreen, 500);
-    }
-  }
-
-  setFullscreen();
+(async function() {
+  var css = document.createElement('style');
+  css.type = 'text/css';
+  css.innerHTML = `
+::-webkit-scrollbar {
+  display: none;
+}
+#template_container,
+#showmedia_video_box_wide {
+  min-height: unset !important;
+  max-height: unset !important;
+  height: unset !important;
+  width: 100% !important;
+  max-width: 100% !important;
+  padding-left: 0 !important;
+  padding-right: 0 !important;
+  margin-left: 0 !important;
+  margin-right: 0 !important;
+}
+#showmedia_video_box_wide {
+  height: 100vh !important;
+}
+#main_content {
+  width: 70% !important;
+}
+#sidebar {
+  width: 30% !important;
+}`;
+  document.head.appendChild(css);
+  await new Promise(resolve => setTimeout(resolve, 500));
+  document.getElementById("showmedia_video").scrollIntoView(true);
 })();
--- tampermonkey/FunimationFullscreen.user.js
+++ tampermonkey/FunimationFullscreen.user.js
@@ -2,31 +2,42 @@
 // @name         Funimation Fullscreen
 // @namespace    https://github.com/Pluc15
 // @author       Pluc
-// @match        *://www.funimation.com/shows/*/*/*/?*marathon=true
+// @match        *://www.funimation.com/shows/*/*/*
 // @grant        none
 // ==/UserScript==
-(function() {
-  console.log("= Funimation Fullscreen Loaded =");
-  function setFullscreen() {
-    var player = document.getElementById("player");
-
-    if (player) {
-      var body = document.querySelector("body");
-      body.style.overflow = "hidden";
-
-      player.style.position = "absolute";
-      player.style.top = 0;
-      player.style.right = 0;
-      player.style.bottom = 0;
-      player.style.left = 0;
-      player.style.zIndex = 999998;
-      player.parentElement.removeChild(player);
-      body.appendChild(player);
-    } else {
-      console.log("Video frame not found, trying again in 500ms");
-      setTimeout(setFullscreen, 500);
-    }
-  }
-
-  setFullscreen();
+(async function() {
+  var css = document.createElement('style');
+  css.type = 'text/css';
+  css.innerHTML = `
+::-webkit-scrollbar {
+  display: none;
+}
+header {
+  position: initial !important;
+}
+#main {
+  margin-top: 0 !important;
+}
+#main,
+section.video-player-section,
+section.video-player-section > .container,
+section.video-player-section > .container > .row,
+section.video-player-section > .container > .row > .video-player-container
+{
+  min-height: unset !important;
+  max-height: unset !important;
+  height: unset !important;
+  max-width: 100% !important;
+  width: 100% !important;
+  padding-left: 0 !important;
+  padding-right: 0 !important;
+  margin-left: 0 !important;
+  margin-right: 0 !important;
+}
+#player {
+  height: 100vh !important;
+}`;
+  document.head.appendChild(css);
+  await new Promise(resolve => setTimeout(resolve, 2000));
+  document.getElementById("player").scrollIntoView(true);
 })();
