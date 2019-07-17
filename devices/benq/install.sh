777089f5e801862e068f91f59c5de5e6dcd39564
Pier-Luc Michaud <pluc.m@outlook.com>
2019-07-17 19:12:16 -0400

    Copy instead of link machine files since they should be available for all users while dotfiles should only be current user (and clones in user home)

--- devices/benq/install.sh
+++ devices/benq/install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ln -fs "$DOTFILES/devices/benq/.monitor-merge" "/usr/local/bin/.monitor-merge"
-ln -fs "$DOTFILES/devices/benq/.monitor-split" "/usr/local/bin/.monitor-split"
+sudo cp "$DOTFILES/devices/benq/.monitor-merge" "/usr/local/bin/.monitor-merge"
+sudo cp "$DOTFILES/devices/benq/.monitor-split" "/usr/local/bin/.monitor-split"
--- devices/steelseries/install.sh
+++ devices/steelseries/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ln -fs "$DOTFILES/devices/steelseries/steelseries-arctis-input.conf" "/usr/share/pulseaudio/alsa-mixer/paths/steelseries-arctis-input.conf"
-ln -fs "$DOTFILES/devices/steelseries/steelseries-arctis-output-mono.conf" "/usr/share/pulseaudio/alsa-mixer/paths/steelseries-arctis-output-mono.conf"
-ln -fs "$DOTFILES/devices/steelseries/steelseries-arctis-output-stereo.conf" "/usr/share/pulseaudio/alsa-mixer/paths/steelseries-arctis-output-stereo.conf"
-ln -fs "$DOTFILES/devices/steelseries/steelseries-arctis-usb-audio.conf" "/usr/share/pulseaudio/alsa-mixer/profile-sets/steelseries-arctis-usb-audio.conf"
+sudo cp "$DOTFILES/devices/steelseries/steelseries-arctis-input.conf" "/usr/share/pulseaudio/alsa-mixer/paths/steelseries-arctis-input.conf"
+sudo cp "$DOTFILES/devices/steelseries/steelseries-arctis-output-mono.conf" "/usr/share/pulseaudio/alsa-mixer/paths/steelseries-arctis-output-mono.conf"
+sudo cp "$DOTFILES/devices/steelseries/steelseries-arctis-output-stereo.conf" "/usr/share/pulseaudio/alsa-mixer/paths/steelseries-arctis-output-stereo.conf"
+sudo cp "$DOTFILES/devices/steelseries/steelseries-arctis-usb-audio.conf" "/usr/share/pulseaudio/alsa-mixer/profile-sets/steelseries-arctis-usb-audio.conf"
