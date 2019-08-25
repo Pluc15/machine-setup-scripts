74f58d51395d29f3c4e55caa9cb8b6b4522a13de
Pier-Luc Michaud <pluc.m@outlook.com>
2019-08-25 19:34:04 -0400

    Fix steelseries installer (missing file)

--- devices/steelseries/install.sh
+++ devices/steelseries/install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-sudo cp "$DOTFILES/devices/steelseries/steelseries-arctis-input.conf" "/usr/share/pulseaudio/alsa-mixer/paths/steelseries-arctis-input.conf"
-sudo cp "$DOTFILES/devices/steelseries/steelseries-arctis-output-mono.conf" "/usr/share/pulseaudio/alsa-mixer/paths/steelseries-arctis-output-mono.conf"
-sudo cp "$DOTFILES/devices/steelseries/steelseries-arctis-output-stereo.conf" "/usr/share/pulseaudio/alsa-mixer/paths/steelseries-arctis-output-stereo.conf"
-sudo cp "$DOTFILES/devices/steelseries/steelseries-arctis-usb-audio.conf" "/usr/share/pulseaudio/alsa-mixer/profile-sets/steelseries-arctis-usb-audio.conf"
+sudo cp "$DOTFILES/devices/steelseries/90-pulseaudio-steelseries-arctis.rules" "/lib/udev/rules.d/"
+sudo cp "$DOTFILES/devices/steelseries/steelseries-arctis-input.conf" "/usr/share/pulseaudio/alsa-mixer/paths/"
+sudo cp "$DOTFILES/devices/steelseries/steelseries-arctis-output-mono.conf" "/usr/share/pulseaudio/alsa-mixer/paths/"
+sudo cp "$DOTFILES/devices/steelseries/steelseries-arctis-output-stereo.conf" "/usr/share/pulseaudio/alsa-mixer/paths/"
+sudo cp "$DOTFILES/devices/steelseries/steelseries-arctis-usb-audio.conf" "/usr/share/pulseaudio/alsa-mixer/profile-sets/"
