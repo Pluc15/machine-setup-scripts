169dbac0bc8406128615cbbe398bcdbc93d20b51
Pier-Luc Michaud <pluc.m@outlook.com>
2020-04-01 18:04:00 -0400

    Make startup more resiliant to one app failing (keep going with the others, dont lock)

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,10 +1,11 @@
-# Single run commands
-nitrogen --restore
-
 # Services
-compton -b
-x11vnc -display :0 -usepw -forever -bg
+dunst &
+compton -b &
+x11vnc -display :0 -usepw -forever -bg &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+
+# Single run commands
+nitrogen --restore &
 
 # Systrays
 remmina -i &
